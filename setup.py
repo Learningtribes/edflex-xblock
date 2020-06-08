90f21461f9a32ac76cc7acb06ac696cd84a5a2ed
slidemoon <moon93110304@gmail.com>
2020-06-08 10:28:26 +0800

    define 'web-fragments' version

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(
     ],
     install_requires=[
         'XBlock==1.2.9',
-        'lxml==3.8.0'
+        'lxml==3.8.0',
+        'web-fragments==0.2.2',
     ],
     entry_points={
         'xblock.v1': [
