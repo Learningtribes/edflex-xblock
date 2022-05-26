e27ad736e6156fa28eb8cf88e59dc9acc6928eec
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2022-05-26 13:26:28 +0800

    added

--- edflex/edflex.py
+++ edflex/edflex.py
@@ -23,7 +23,7 @@ class EdflexXBlock(StudioEditableXBlockMixin, XBlock):
 
     display_name = String(
         scope=Scope.settings,
-        default=_("External Resource")
+        default=_("Currated Content")
     )
     format = String(scope=Scope.settings)
     category = String(scope=Scope.settings)
