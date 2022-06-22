ee3ef05e2779a9d7db71c7771ae420ad6e71701e
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2022-06-22 18:02:36 +0800

    fixed: wrong spell 'Currated' ---> 'Curated'

--- edflex/edflex.py
+++ edflex/edflex.py
@@ -23,7 +23,7 @@ class EdflexXBlock(StudioEditableXBlockMixin, XBlock):
 
     display_name = String(
         scope=Scope.settings,
-        default=_("Currated Content")
+        default=_("Curated Content")
     )
     format = String(scope=Scope.settings)
     category = String(scope=Scope.settings)
